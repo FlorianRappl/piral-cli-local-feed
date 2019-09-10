f674144e18e710825c1a7d4e817bbf56b1e59a10
Florian Rappl <florian.rappl@smapiot.com>
2019-09-10 05:50:11 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # piral-cli-local-feed
-Adds a command to run a local feed service.
+
+Plugin for the `piral-cli`. Adds a command to run a local feed service.
+
+## Installation
+
+Install the plugin either locally or globally.
+
+We recommend the local installation:
+
+```sh
+npm i piral-cli-local-feed
+```
+
+## Running Standalone
+
+Running it standalone can be done using the `piral` command:
+
+```sh
+piral local-feed
+```
+
+Alternatively, run it with the `pb local-feed` command.
+
+Regarding the available options we currently support two flags:
+
+- **port**, by default 9000
+- **api-key**, by default a pre-defined set of API keys (displayed in the command line)
+
+The API key is the one being used when uploading a pilet using the `piral-cli`. Multiple keys could be given when using comma as separators.
+
+## During Debug
+
+Running it during a debug session of Piral is also possible.
+
+```sh
+piral debug <regular options> --feed ... --feed-port ... --feed-api-key ...
+```
+
+This will run the local feed service using the same options as above, just on-the-side and prefixed with `feed`.
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { CliPlugin } from 'piral-cli';
+import { runApp } from 'sample-pilet-service';
+
+function apiKeys(arg: string) {
+  return (arg && arg.split(',')) || undefined;
+}
+
+const plugin: CliPlugin = (cli) => {
+  cli.withCommand({
+    name: 'local-feed-piral',
+    alias: ['local-feed'],
+    description: 'Starts a local pilet feed service for development purposes.',
+    arguments: [],
+    flags(argv) {
+      return argv
+        .number('port')
+        .describe('port', 'The port of the local pilet feed service.')
+        .default('port', 9000)
+        .string('api-key')
+        .describe('api-key', 'The custom API key to use. Comma separated for multiple entries.')
+        .default('api-key', '')
+        .string('base')
+        .default('base', process.cwd())
+        .describe('base', 'Sets the base directory. By default the current directory is used.');
+    },
+    run(args) {
+      return new Promise(resolve => {
+        const server = runApp({
+          port: args.port as number,
+          apiKeys: apiKeys(args.apiKey as string),
+        });
+        server.on('close', resolve);
+      });
+    },
+  });
+
+  cli.beforeCommand('debug-piral', args => {
+    if (args.feed) {
+      runApp({
+        port: args.feedPort as number,
+        apiKeys: apiKeys(args.feedApiKey as string),
+      });
+    }
+  });
+};
+
+module.exports = plugin;
